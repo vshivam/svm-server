42d296a218bc42bb29972e485c4539b810d42ed1
Shivam <shivam.bitsgoa@gmail.com>
2019-02-04 14:03:58 +0100

    fix key manager

---
+++ key_manager.py
@@ -0,0 +1,21 @@
+from app.models import APIKey
+from app import create_app
+from app import db
+
+app = create_app()
+db.init_app(app)
+
+def generate_new_key():
+    with app.app_context():
+        key = APIKey()
+        key.save_to_db()
+        print key.str
+
+def show_keys():
+    with app.app_context():
+        for key in APIKey.query.all():
+            print key.str
+
+
+if __name__ == '__main__':
+    print ('This is the key manager. Use it to create new keys or list all')
