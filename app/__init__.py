8157e00ca799b53fe55f205b383528b9da800caa
Shivam Verma <shivam.bitsgoa@gmail.com>
2019-02-13 00:48:38 +0100

    added endpoint for entity extraction

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_api import FlaskAPI, status
 from flask_sqlalchemy import SQLAlchemy
 from sklearn.externals import joblib
 
+from ner import get_ner
+
 db = SQLAlchemy()
 
 
@@ -72,9 +74,38 @@ def create_app():
                 for i, class_ in enumerate(model.classes_):
                     output['probs'][class_] = probs[0][i] * 100
 
+            # check if entities are requested
+            need_entities = request.args.get('needEntities')
+            need_entities = need_entities == 'True' or need_entities == 'true'
+            if need_entities:
+                output['entities'] = get_ner(utterance)
             # save to db
             u = Utterances(utterance)
             u.save_to_db()
             return output, status.HTTP_200_OK
+
+    @app.route("/entities", methods=['POST'])
+    def ner():
+        # check if entities are requested
+        request_key = request.args.get("api_key")
+        api_key = APIKey.query.filter(APIKey.str == request_key).first()
+
+        # is api key valid ?
+        if api_key is None:
+            return {
+                       'success': False,
+                       'message': 'Invalid or missing api_key'
+                   }, status.HTTP_401_UNAUTHORIZED
+
+        # does request have an utterance param ?
+        utterance = request.args.get("utterance")
+        if utterance is None:
+            return {
+                'success': False,
+                'message': 'missing POST param: utterance'
+            }
+
+        output = {'success': True, 'utterance': utterance, 'entities': get_ner(utterance)}
+        return output, status.HTTP_200_OK
 
     return app
--- ner.py
+++ ner.py
@@ -1,15 +1,24 @@
-import spacy 
+import spacy
+
 nlp = spacy.load('en')
 
+''' the following is a solution to make spacy's entity recognition work with
+smaller case inputs, e.g., berlin (as opposed to Berlin) 
+
+See here: https://github.com/explosion/spaCy/issues/701#issuecomment-274653065
+'''
+
 probs = {w.orth: w.prob for w in nlp.vocab}
-usually_titled = [w for w in nlp.vocab if w.is_title and probs.get(nlp.vocab[w.orth].lower, -10000) < probs.get(w.orth, -10000)]
+usually_titled = [w for w in nlp.vocab if
+                  w.is_title and probs.get(nlp.vocab[w.orth].lower, -10000) < probs.get(w.orth, -10000)]
 
 for lex in usually_titled:
-   lower = nlp.vocab[lex.lower]
-   lower.shape = lex.shape
-   lower.is_title = lex.is_title
-   lower.cluster = lex.cluster
-   lower.is_lower = lex.is_lower
+    lower = nlp.vocab[lex.lower]
+    lower.shape = lex.shape
+    lower.is_title = lex.is_title
+    lower.cluster = lex.cluster
+    lower.is_lower = lex.is_lower
+
 
 def get_ner(utterance):
     doc = nlp(utterance.decode('utf_8'))
@@ -19,14 +28,16 @@ def get_ner(utterance):
     for e in doc.ents:
         print e.label_
         if e.label_ == 'GPE':
-            locations.append(e.text.encode('utf_8'))    
+            locations.append(e.text.encode('utf_8'))
         elif e.label_ == 'DATE' or e.label_ == 'TIME':
             time.append(e.text.encode('utf_8'))
-    print locations
-    print time
+    return {
+        'time': time,
+        'location': locations
+    }
+
 
 if __name__ == '__main__':
     get_ner('hey! what is the weather like in berlin tomorrow ?')
     get_ner('berlin new delhi today and tomorrow')
     get_ner('berlin new delhi today tomorrow')
-
